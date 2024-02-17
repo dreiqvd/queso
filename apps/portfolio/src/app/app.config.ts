c69681af1efd32939b644cb561f032a98ec0bc6f
Drei Quevada <me@dreiq.dev>
2024-02-18 01:22:12 +0800

    setup: create a router strategy for page title

--- apps/portfolio/src/app/app.config.ts
+++ apps/portfolio/src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, TitleStrategy } from '@angular/router';
+
+import { PageTitleStrategy } from '@queso/utils/strategies';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [
+    provideClientHydration(),
+    provideRouter(appRoutes),
+    {
+      provide: TitleStrategy,
+      useClass: PageTitleStrategy,
+    },
+  ],
 };
