17e9675e47a61c3b2c8b4a09172e8ae0c75f9d5b
Drei Quevada <me@dreiq.dev>
2024-02-18 14:19:10 +0800

    refactor: convert shared directory into angular library

---
+++ libs/shared/src/constants.ts
@@ -0,0 +1,35 @@
+/** These breakpoints should be synced with _breakpoints.scss */
+export const BREAKPOINTS = {
+  MOBILE_SM: 360,
+  MOBILE_MD: 480,
+  TABLET_SM: 640,
+  TABLET_MD: 768,
+  DESKTOP_SM: 1024,
+  DESKTOP_MD: 1366,
+  DESKTOP_LG: 1919,
+  XL_WIDTH: 1920,
+  SM_HEIGHT: 768,
+};
+
+export const SOCIAL_LINKS = [
+  {
+    link: 'https://www.linkedin.com/in/dreiqvd',
+    name: 'LinkedIn',
+    icon: 'linkedin',
+  },
+  {
+    link: 'https://github.com/dreiqvd',
+    name: 'GitHub',
+    icon: 'github',
+  },
+  {
+    link: 'https://twitter.com/dreiqdev',
+    name: 'Twitter',
+    icon: 'twitter',
+  },
+  {
+    link: 'https://dribbble.com/dreiqvd',
+    name: 'Dribbble',
+    icon: 'dribbble',
+  },
+];
