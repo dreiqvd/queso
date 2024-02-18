8cfb8bac27e4ba7ac241dc16813b6505a6633e83
Drei Quevada <me@dreiq.dev>
2024-02-18 15:11:09 +0800

    feat: add header blob in hero section

--- apps/portfolio/src/app/pages/home/sections/home-hero/home-hero.component.ts
+++ apps/portfolio/src/app/pages/home/sections/home-hero/home-hero.component.ts
@@ -1,5 +1,13 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, OnInit, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { fromEvent } from 'rxjs';
 
@@ -13,17 +21,24 @@ import { getViewportWidth } from '@queso/utils';
   templateUrl: './home-hero.component.html',
   styleUrl: './home-hero.component.scss',
 })
-export class HomeHeroComponent implements OnInit {
+export class HomeHeroComponent implements OnInit, AfterViewInit {
+  @ViewChild('headerBlobWrapper') headerBlobWrapper!: ElementRef<HTMLElement>;
+  @ViewChild('headerBlobAnchor') headerBlobAnchor!: ElementRef<HTMLElement>;
+
   readonly greetingText1 = 'Hi, I am'.split('');
   readonly greetingText2 = 'Drei'.split('');
   readonly shouldBreakText = signal(false);
 
-  constructor(private platformService: PlatformService) {
+  constructor(
+    private renderer: Renderer2,
+    private platformService: PlatformService
+  ) {
     if (this.platformService.isUsingBrowser()) {
       fromEvent(window, 'resize')
         .pipe(takeUntilDestroyed())
         .subscribe(() => {
           this.checkTextBreak();
+          this.repositionBlob();
         });
     }
   }
@@ -32,10 +47,27 @@ export class HomeHeroComponent implements OnInit {
     this.checkTextBreak();
   }
 
+  ngAfterViewInit(): void {
+    this.repositionBlob();
+  }
+
   /** Checks whether to apply line break to greeting text */
   private checkTextBreak(): void {
     if (this.platformService.isUsingBrowser()) {
       this.shouldBreakText.set(getViewportWidth() <= BREAKPOINTS.TABLET_SM);
+    }
+  }
+
+  /** Change positioning of the header blob */
+  private repositionBlob(): void {
+    if (this.platformService.isUsingBrowser()) {
+      const blobAnchorEl = this.headerBlobAnchor.nativeElement;
+      const blobWrapperEl = this.headerBlobWrapper.nativeElement;
+      let left = blobAnchorEl.getBoundingClientRect().left;
+      if (getViewportWidth() <= BREAKPOINTS.MOBILE_MD) {
+        left -= 10;
+      }
+      this.renderer.setStyle(blobWrapperEl, 'left', `${left}px`);
     }
   }
 }
