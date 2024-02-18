04bf107473fe0d3e5e086ffaedc573aa4bbbba47
Drei Quevada <me@dreiq.dev>
2024-02-18 14:00:13 +0800

    feat: add hero section component

---
+++ apps/portfolio/src/app/pages/home/sections/home-hero/home-hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qs-home-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-hero.component.html',
+  styleUrl: './home-hero.component.scss',
+})
+export class HomeHeroComponent {}
