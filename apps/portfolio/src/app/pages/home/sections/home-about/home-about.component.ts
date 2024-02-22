f7d6d2761c29f7a68819edd0df52990d2a125f17
Drei Quevada <me@dreiq.dev>
2024-02-22 18:49:20 +0800

    feat: add side content to about section

--- apps/portfolio/src/app/pages/home/sections/home-about/home-about.component.ts
+++ apps/portfolio/src/app/pages/home/sections/home-about/home-about.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  signal,
+} from '@angular/core';
 import { MatTooltip } from '@angular/material/tooltip';
 
 import { IconComponent } from '@queso/ui-kit/icon';
@@ -6,8 +13,38 @@ import { IconComponent } from '@queso/ui-kit/icon';
 @Component({
   selector: 'qs-home-about',
   standalone: true,
-  imports: [IconComponent, MatTooltip],
+  imports: [IconComponent, MatTooltip, NgOptimizedImage],
   templateUrl: './home-about.component.html',
   styleUrl: './home-about.component.scss',
 })
-export class HomeAboutComponent {}
+export class HomeAboutComponent {
+  @ViewChild('portraitBlob') portraitBlob!: ElementRef<HTMLElement>;
+  @ViewChild('portraitImg') portraitImg!: ElementRef<HTMLElement>;
+  @ViewChild('sideContentRef') sideContentRef!: ElementRef<HTMLElement>;
+
+  readonly isPortraitBlobVisible = signal(false);
+
+  constructor(private renderer: Renderer2) {}
+
+  /** Reposition portrait blob in relation to the portrait image position */
+  private repositionPortraitBlob(): void {
+    const portraitImg = this.portraitImg.nativeElement;
+    const portraitBlob = this.portraitBlob.nativeElement;
+    const portraitImgHeight = portraitImg.clientHeight;
+    const portraitBlobHeight = portraitBlob.clientHeight;
+    const diff = portraitImgHeight - portraitBlobHeight;
+    const bottom = (diff >= 0 ? diff : 0) + 40;
+    this.renderer.setStyle(portraitBlob, 'bottom', `${bottom}px`);
+    this.renderer.setStyle(
+      this.sideContentRef.nativeElement,
+      'height',
+      `${portraitBlobHeight + bottom}px`
+    );
+    this.isPortraitBlobVisible.set(true);
+  }
+
+  /** Handles event when the portrait image has been fetched */
+  onPortraitImgLoad(): void {
+    this.repositionPortraitBlob();
+  }
+}
