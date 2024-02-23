dd6a2f7cb2cc8274a18e7927761a9cded1fe2f03
Drei Quevada <me@dreiq.dev>
2024-02-23 17:08:27 +0800

    feat: update contents of home skills section

--- apps/portfolio/src/app/pages/home/sections/home-skills/home-skills.component.ts
+++ apps/portfolio/src/app/pages/home/sections/home-skills/home-skills.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { IconComponent } from '@queso/ui-kit/icon';
+
 @Component({
   selector: 'qs-home-skills',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './home-skills.component.html',
   styleUrl: './home-skills.component.scss',
 })
@@ -11,5 +13,19 @@ export class HomeSkillsComponent {
   readonly coreSkills = [
     { logo: 'angular', name: 'Angular' },
     { logo: 'node', name: 'Node.js' },
+  ];
+  readonly otherTools = [
+    {
+      key: 'FRONTEND',
+      items: ['react', 'tailwind', 'rxjs', 'ngrx', 'cypress', 'typescript'],
+    },
+    {
+      key: 'BACKEND',
+      items: ['python', 'postgres', 'mongodb', 'graphql'],
+    },
+    {
+      key: 'OTHERS',
+      items: ['google-maps', 'gcloud', 'docker', 'firebase', 'nx'],
+    },
   ];
 }
