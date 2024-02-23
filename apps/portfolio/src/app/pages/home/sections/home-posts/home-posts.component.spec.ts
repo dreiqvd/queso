cf79d51acdabc04ace860d12dafd2e07bc2b2a0f
Drei Quevada <me@dreiq.dev>
2024-02-24 00:58:59 +0800

    feat: add contents to posts section

--- apps/portfolio/src/app/pages/home/sections/home-posts/home-posts.component.spec.ts
+++ apps/portfolio/src/app/pages/home/sections/home-posts/home-posts.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { HomePostsComponent } from './home-posts.component';
 
 describe('HomePostsComponent', () => {
--- apps/portfolio/src/app/pages/home/sections/home-posts/home-posts.component.ts
+++ apps/portfolio/src/app/pages/home/sections/home-posts/home-posts.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { IconComponent } from '@queso/ui-kit/icon';
+import { PillComponent } from '@queso/ui-kit/pill';
 
 @Component({
   selector: 'qs-home-posts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [IconComponent, PillComponent],
   templateUrl: './home-posts.component.html',
   styleUrl: './home-posts.component.scss',
 })
-export class HomePostsComponent {}
+export class HomePostsComponent {
+  readonly posts = [
+    {
+      title: 'AI Utility Tools For Your Web Project',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      tag: 'listicle',
+      date: 'Feb. 02, 2024',
+    },
+    {
+      title: "GitHub CoPilot: Why It's Worth the Pay",
+      excerpt:
+        'Maecenas vulputate, eros vitae gravida pellentesque, turpis ipsum.',
+      tag: 'tech',
+      date: 'Oct. 16, 2023',
+    },
+    {
+      title: 'My Bookmarks as a Web Developer',
+      excerpt:
+        'Cras ultricies hendrerit vehicula. Fusce nulla lacus, laoreet vel.',
+      tag: 'listicle',
+      date: 'Sep. 24, 2023',
+    },
+  ];
+}
