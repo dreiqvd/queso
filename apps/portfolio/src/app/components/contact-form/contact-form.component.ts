5796b5142a540d01c8dd6450f0e3b6ba7bc051a8
Drei Quevada <me@dreiq.dev>
2024-03-03 15:40:31 +0800

    feat: prepare initial implementation of a generic contact form

--- apps/portfolio/src/app/components/contact-form/contact-form.component.ts
+++ apps/portfolio/src/app/components/contact-form/contact-form.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { AnimationsDirective } from '@queso/common';
+import { InputComponent } from '@queso/ui-kit/input';
+import { TextareaComponent } from '@queso/ui-kit/textarea';
 
 @Component({
   selector: 'qs-contact-form',
   standalone: true,
-  imports: [],
+  imports: [
+    NgTemplateOutlet,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    InputComponent,
+    TextareaComponent,
+    AnimationsDirective,
+  ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss',
 })
-export class ContactFormComponent {}
+export class ContactFormComponent implements OnInit {
+  readonly contactForm = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    subject: new FormControl(null, Validators.required),
+    message: new FormControl(null, Validators.required),
+  });
+
+  ngOnInit(): void {
+    this.contactForm.valueChanges.subscribe((value) => {
+      // console.log(value);
+    });
+  }
+
+  onSubmitForm(): void {
+    console.log(this.contactForm.value);
+  }
+}
--- apps/portfolio/src/app/pages/home/sections/home-contact/home-contact.component.spec.ts
+++ apps/portfolio/src/app/pages/home/sections/home-contact/home-contact.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { HomeContactComponent } from './home-contact.component';
 
 describe('HomeContactComponent', () => {
