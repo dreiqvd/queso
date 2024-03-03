f68a0d958205e302c6e767453033fa75d8d2e98c
Drei Quevada <me@dreiq.dev>
2024-03-03 15:38:29 +0800

    feat: implement a more dynamic approach for handling mat error messages

--- libs/ui-kit/src/directives/generic-field.directive.ts
+++ libs/ui-kit/src/directives/generic-field.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, Input, OnInit } from '@angular/core';
 import { ControlContainer, FormControl, FormGroup } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Directive({
   selector: '[qsFormControl]',
@@ -26,6 +27,15 @@ export class GenericFormFieldDirective implements OnInit {
   @Input() hideRequiredMarker = false;
 
   /**
+   * The optional mapping for the error messages.
+   * @example { required: 'Your custom required error message.' }
+   */
+  @Input() errorMsgMap: { [key: string]: string } = {};
+
+  /** Holds the value of the error message string */
+  errorMessage?: string;
+
+  /**
    * The resolved form control instance to be used in the directive.
    */
   public formControl!: FormControl;
@@ -45,5 +55,62 @@ export class GenericFormFieldDirective implements OnInit {
       const group = this.controlContainer.control as FormGroup;
       this.formControl = group.get(this.control) as FormControl;
     }
+
+    this.resolveValidationHandler();
+  }
+
+  /**
+   * Subscribe to the status changes of the form control and resolve the error message
+   */
+  private resolveValidationHandler(): void {
+    this.formControl.statusChanges
+      .pipe(debounceTime(500))
+      .subscribe((state) => {
+        if (state === 'INVALID' && this.formControl.errors) {
+          // At the moment, only one error message is displayed within the field.
+          const errorKey = Object.keys(this.formControl.errors)[0];
+          let errorMsg = '';
+          if (this.errorMsgMap[errorKey]) {
+            errorMsg = this.errorMsgMap[errorKey];
+          } else if (
+            DEFAULT_ERROR_MESSAGES[
+              errorKey as keyof typeof DEFAULT_ERROR_MESSAGES
+            ]
+          ) {
+            errorMsg =
+              DEFAULT_ERROR_MESSAGES[
+                errorKey as keyof typeof DEFAULT_ERROR_MESSAGES
+              ];
+          }
+
+          if (this.errorMessage !== errorMsg) {
+            this.errorMessage = errorMsg;
+          }
+        }
+      });
+  }
+
+  /**
+   * Compute the value of the "required" field error message.
+   */
+  public checkRequiredMsg(): void {
+    if (this.formControl.hasError('required')) {
+      const errorMsg = this.errorMsgMap['required']
+        ? this.errorMsgMap['required']
+        : DEFAULT_ERROR_MESSAGES['required'];
+      // Only assign new error message if it was not the same with the previous one.
+      if (errorMsg !== this.errorMessage) {
+        this.errorMessage = errorMsg;
+      }
+    }
+  }
+
+  /** Generic handle for blur event */
+  public onBlur(): void {
+    this.checkRequiredMsg();
   }
 }
+
+const DEFAULT_ERROR_MESSAGES = {
+  required: 'This field is required.',
+} as const;
