7dbafda7f9a660994e600bc34d9cd68cae4faea2
Drei Quevada <me@dreiq.dev>
2024-03-09 00:59:50 +0800

    refactor: update responsive styling due to change in section heights

--- apps/portfolio/src/app/pages/home/sections/home-posts/home-posts.component.ts
+++ apps/portfolio/src/app/pages/home/sections/home-posts/home-posts.component.ts
@@ -1,14 +1,9 @@
-import { AfterViewInit, Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatTooltip } from '@angular/material/tooltip';
 
-import {
-  AnimationsDirective,
-  BREAKPOINTS,
-  PlatformService,
-} from '@queso/common';
+import { AnimationsDirective } from '@queso/common';
 import { IconComponent } from '@queso/ui-kit/icon';
 import { PillComponent } from '@queso/ui-kit/pill';
-import { getViewportHeight, getViewportWidth } from '@queso/utils';
 
 @Component({
   selector: 'qs-home-posts',
@@ -17,7 +12,7 @@ import { getViewportHeight, getViewportWidth } from '@queso/utils';
   templateUrl: './home-posts.component.html',
   styleUrl: './home-posts.component.scss',
 })
-export class HomePostsComponent implements AfterViewInit {
+export class HomePostsComponent {
   readonly posts = [
     {
       title: 'AI Utility Tools For Your Web Project',
@@ -40,29 +35,4 @@ export class HomePostsComponent implements AfterViewInit {
       date: 'Sep. 24, 2023',
     },
   ];
-
-  /** Determines whether to show link for the blogs page */
-  showBlogsLink = true;
-
-  constructor(private platformService: PlatformService) {}
-
-  ngAfterViewInit(): void {
-    if (this.platformService.isUsingBrowser()) {
-      // Only show blogs link if viewport height is greater than or equal to 900px
-      setTimeout(() => this.checkBlogsLinkVisibility());
-    }
-  }
-
-  @HostListener('window:resize')
-  onWindowResize(): void {
-    this.checkBlogsLinkVisibility();
-  }
-
-  private checkBlogsLinkVisibility(): void {
-    const height = getViewportHeight();
-    this.showBlogsLink =
-      getViewportWidth() <= BREAKPOINTS.DESKTOP_SM ||
-      height >= 920 ||
-      height <= BREAKPOINTS.SM_HEIGHT;
-  }
 }
