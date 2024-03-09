82f847d84df73aa239eea419ff4e2c41adb7e6ae
Drei Quevada <me@dreiq.dev>
2024-03-10 01:59:30 +0800

    feat: set up animations in contact page

--- apps/portfolio/src/app/pages/contact/contact.component.ts
+++ apps/portfolio/src/app/pages/contact/contact.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { AnimationsDirective } from '@queso/common';
+
 import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
 import { NavbarComponent } from '../../components/navbar';
 
 @Component({
   selector: 'qs-contact',
   standalone: true,
-  imports: [NavbarComponent, ContactFormComponent],
+  imports: [NavbarComponent, ContactFormComponent, AnimationsDirective],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
