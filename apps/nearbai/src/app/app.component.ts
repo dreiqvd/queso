3e90695c056dde71f45189a6755f0416acb7bce7
Drei Quevada <me@dreiq.dev>
2024-04-01 02:23:32 +0800

    chore: apply initial code cleanup

--- apps/nearbai/src/app/app.component.ts
+++ apps/nearbai/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'queso-root',
+  imports: [RouterOutlet],
+  selector: 'qs-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'nearbai';
