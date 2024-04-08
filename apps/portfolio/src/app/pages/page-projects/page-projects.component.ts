85858fb3d0d243993a3937d470fa403d6df3c346
Drei Quevada <me@dreiq.dev>
2024-04-08 16:51:26 +0800

    feat: prepare initial update for projects page

--- apps/portfolio/src/app/pages/page-projects/page-projects.component.ts
+++ apps/portfolio/src/app/pages/page-projects/page-projects.component.ts
@@ -4,6 +4,7 @@ import { RouterLink } from '@angular/router';
 
 import { AnimationsDirective } from '@queso/common/directives';
 import { IconComponent } from '@queso/ui-kit/icon';
+import { PillComponent } from '@queso/ui-kit/pill';
 
 import { PageContainerComponent } from '../../shared/page-container';
 
@@ -16,7 +17,19 @@ import { PageContainerComponent } from '../../shared/page-container';
     PageContainerComponent,
     AnimationsDirective,
     IconComponent,
+    PillComponent,
   ],
   templateUrl: './page-projects.component.html',
 })
-export class PageProjectsComponent {}
+export class PageProjectsComponent {
+  readonly projects = [
+    {
+      key: 'nearbai',
+      name: 'NearBai',
+      excerpt:
+        "A simple place-finder that uses Google Places API to search for nearby places either from a landmark or the user's current location. This is a personal small-scale project to play around with Google Cloud services.",
+      tags: ['google-api', 'maps', 'angular'],
+      url: 'https://nearbai.dreiq.dev',
+    },
+  ];
+}
