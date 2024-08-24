9f3968054984bf6b01103e004dd7b4843aca74f2
Drei Quevada <me@dreiq.dev>
2024-08-25 00:25:38 +0800

    cashflow: prepare initial theming for cashflow app

--- apps/cashflow/src/app/app.config.ts
+++ apps/cashflow/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
@@ -9,5 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideAnimationsAsync(),
   ],
 };
