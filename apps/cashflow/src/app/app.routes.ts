43fdf1fb772a030fdcd4e4d249a2b74dd72b8505
Drei Quevada <me@dreiq.dev>
2024-08-25 21:25:13 +0800

    cashflow: add auth guard to dashboard page

--- apps/cashflow/src/app/app.routes.ts
+++ apps/cashflow/src/app/app.routes.ts
@@ -1,7 +1,23 @@
+import {
+  AuthGuard,
+  AuthPipe,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
 import { Route } from '@angular/router';
 
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { LoginComponent } from './modules/login/login.component';
+
+const redirectUnauthorizedToLogin = (): AuthPipe =>
+  redirectUnauthorizedTo(['login']);
 
 export const appRoutes: Route[] = [
-  { path: '', pathMatch: 'full', component: DashboardComponent },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
+  },
+  { path: 'login', pathMatch: 'full', component: LoginComponent },
 ];
