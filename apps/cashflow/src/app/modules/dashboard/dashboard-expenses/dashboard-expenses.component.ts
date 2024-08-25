ee8fed6eaa130b01581ffccdb77aa319c20b204d
Drei Quevada <me@dreiq.dev>
2024-08-25 13:15:52 +0800

    cashflow: set up a separate component for expenses table

--- apps/cashflow/src/app/modules/dashboard/dashboard-expenses/dashboard-expenses.component.ts
+++ apps/cashflow/src/app/modules/dashboard/dashboard-expenses/dashboard-expenses.component.ts
@@ -1,44 +1,27 @@
-import { CurrencyPipe, NgTemplateOutlet } from '@angular/common';
 import { afterNextRender, Component, inject } from '@angular/core';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { take } from 'rxjs';
 
-import { QsOrdinalPipe } from '@queso/common/pipes';
 import { QsTabGroupDirective } from '@queso/ui-kit/tabs';
 
 import { Expense } from '../../../models';
 import { ExpenseService } from '../../../services';
 
+import { ExpensesTableComponent } from './expenses-table/expenses-table.component';
+
 @Component({
   selector: 'app-dashboard-expenses',
   standalone: true,
-  imports: [
-    NgTemplateOutlet,
-    CurrencyPipe,
-    MatTableModule,
-    MatTabsModule,
-    QsOrdinalPipe,
-    QsTabGroupDirective,
-  ],
+  imports: [MatTabsModule, QsTabGroupDirective, ExpensesTableComponent],
   templateUrl: './dashboard-expenses.component.html',
   styleUrl: './dashboard-expenses.component.scss',
 })
 export class DashboardExpensesComponent {
   private readonly expenseService = inject(ExpenseService);
 
-  readonly tblColumns = [
-    'name',
-    'amount',
-    'dueDate',
-    'lastPaymentDate',
-    'paymentAccount',
-    'isPaid',
-    'action',
-  ];
-  readonly period1ExpensesDataSource = new MatTableDataSource<IExpenseRow>();
-  readonly period2ExpensesDataSource = new MatTableDataSource<IExpenseRow>();
   readonly selectedPeriodIndex = new Date().getDate() > 15 ? 0 : 1;
+  period1Expenses: Expense[] = [];
+  period2Expenses: Expense[] = [];
 
   expenses: Expense[] = [];
 
@@ -49,45 +32,9 @@ export class DashboardExpensesComponent {
         .pipe(take(1))
         .subscribe((expenses) => {
           const data = expenses.sort((a, b) => b.amount - a.amount);
-          this.period1ExpensesDataSource.data = this.getPeriodExpenses(data, 1);
-          this.period2ExpensesDataSource.data = this.getPeriodExpenses(data, 2);
+          this.period1Expenses = data.filter((d) => d.period === 1);
+          this.period2Expenses = data.filter((d) => d.period === 2);
         });
     });
   }
-
-  private getPeriodExpenses(data: Expense[], period: number): IExpenseRow[] {
-    return data
-      .filter((e) => e.period === period)
-      .map((e) => this.mapExpenseToRow(e));
-  }
-
-  private mapExpenseToRow(expense: Expense): IExpenseRow {
-    const {
-      name,
-      amount,
-      paymentDay,
-      lastPaymentDate,
-      isPaid,
-      paymentAccount,
-    } = expense;
-    return {
-      name,
-      amount,
-      dueDate: paymentDay,
-      lastPaymentDate: lastPaymentDate?.toDateString() ?? 'N/A',
-      paymentAccount,
-      isPaid: !!isPaid,
-      action: 'Pay',
-    };
-  }
-}
-
-interface IExpenseRow {
-  name: string;
-  amount: number;
-  dueDate: number;
-  lastPaymentDate: string;
-  paymentAccount: string;
-  isPaid: boolean;
-  action: string;
 }
---
+++ apps/cashflow/src/app/modules/dashboard/dashboard-expenses/expenses-table/expenses-table.component.ts
@@ -0,0 +1,75 @@
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, effect, input, ViewChild } from '@angular/core';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
+import { QsOrdinalPipe } from '@queso/common/pipes';
+import { Expense } from 'apps/cashflow/src/app/models';
+
+@Component({
+  selector: 'app-expenses-table',
+  standalone: true,
+  imports: [
+    CurrencyPipe,
+    DatePipe,
+    MatTableModule,
+    MatSortModule,
+    QsOrdinalPipe,
+  ],
+  templateUrl: './expenses-table.component.html',
+  styleUrl: './expenses-table.component.scss',
+})
+export class ExpensesTableComponent {
+  @ViewChild(MatSort) sort!: MatSort;
+
+  expenses = input.required<Expense[]>();
+
+  readonly tblColumns = [
+    'name',
+    'amount',
+    'dueDate',
+    'paymentAccount',
+    'lastPaymentDate',
+    'isPaid',
+    'action',
+  ];
+  tblDataSource = new MatTableDataSource<IExpenseRow>();
+
+  constructor() {
+    effect(() => {
+      this.tblDataSource.data = this.expenses().map(this.mapExpenseToRow);
+      this.tblDataSource.sort = this.sort;
+    });
+  }
+
+  private mapExpenseToRow(expense: Expense): IExpenseRow {
+    const {
+      name,
+      amount,
+      paymentDay,
+      lastPaymentDate,
+      isPaid,
+      paymentAccount,
+    } = expense;
+
+    return {
+      name,
+      amount,
+      dueDate: paymentDay,
+      lastPaymentDate,
+      paymentAccount,
+      isPaid: !!isPaid,
+      action: 'Pay',
+    };
+  }
+}
+
+interface IExpenseRow {
+  name: string;
+  amount: number;
+  dueDate: number;
+  lastPaymentDate: Date | undefined;
+  paymentAccount: string;
+  isPaid: boolean;
+  action: string;
+}
