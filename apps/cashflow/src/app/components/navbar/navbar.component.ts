636828381ec190f4dc8d62623bdff336a009b58c
Drei Quevada <me@dreiq.dev>
2024-12-13 12:39:02 +0800

    cashflow: set up navigation menu items

--- apps/cashflow/src/app/components/navbar/navbar.component.ts
+++ apps/cashflow/src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+
+import { IconStyle, QsIconComponent } from '@queso/ui-kit/icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule, QsIconComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   private readonly auth = inject(Auth);
-  private readonly route = inject(Router);
+  private readonly router = inject(Router);
+
+  readonly routes = [
+    {
+      path: '/',
+      label: 'Dashboard',
+      icon: 'dashboard',
+      iconStyle: 'custom' as IconStyle,
+    },
+    {
+      path: '/expenses',
+      label: 'Expenses',
+      icon: 'coins',
+      iconStyle: 'light' as IconStyle,
+    },
+  ];
 
   signOut(): void {
     this.auth.signOut().then(() => {
-      this.route.navigate(['/login']);
+      this.router.navigate(['/login']);
     });
   }
 }
