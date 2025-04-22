58b1fd650c3c08a1140b9f9c121193e70dab63ba
Drei Quevada <me@dreiq.dev>
2025-04-22 14:18:18 +0800

    portfolio2: set up initial components needed for generic page container

---
+++ apps/portfolio2/src/app/components/page-container/page-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-page-container',
+  imports: [NavbarComponent],
+  templateUrl: './page-container.component.html',
+})
+export class PageContainerComponent {}
