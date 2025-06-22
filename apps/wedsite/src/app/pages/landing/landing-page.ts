b89e98f2d6ce0fc2831a7cf08e2b5c050e50415d
Drei Quevada <me@dreiq.dev>
2025-06-22 09:56:30 +0800

    wedsite: set up landing page navigation

--- apps/wedsite/src/app/pages/landing/landing-page.ts
+++ apps/wedsite/src/app/pages/landing/landing-page.ts
@@ -1,38 +1,105 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {
+  afterNextRender,
+  Component,
+  ElementRef,
+  OnDestroy,
+  QueryList,
+  signal,
+  ViewChildren,
+} from '@angular/core';
 
 import { HeroSection } from './hero-section/hero-section';
 
 @Component({
-  imports: [RouterLink, HeroSection],
+  imports: [HeroSection],
   selector: 'app-landing-page',
   templateUrl: './landing-page.html',
+  styles: `
+    .nav-item.active {
+      opacity: 1;
+
+      span {
+        &::before {
+          content: url('/images/asterisk.svg');
+          margin-right: 4px;
+        }
+      }
+    }
+  `,
 })
-export class LandingPage {
-  readonly navItems = [
-    {
-      label: 'Home',
-      fragment: 'home',
-    },
-    {
-      label: 'Details',
-      fragment: 'details',
-    },
-    {
-      label: 'RSVP',
-      fragment: 'rsvp',
-    },
-    {
-      label: 'FAQs',
-      fragment: 'faqs',
-    },
-    {
-      label: 'Registry',
-      fragment: 'registry',
-    },
-    {
-      label: 'Media',
-      fragment: 'media',
-    },
-  ];
+export class LandingPage implements OnDestroy {
+  @ViewChildren('section') sections!: QueryList<ElementRef<HTMLElement>>;
+
+  readonly activeRoute = signal<string>('home');
+
+  readonly navItems = NAV_ITEMS;
+  private intersectionObserver$?: IntersectionObserver;
+
+  constructor() {
+    afterNextRender(() => {
+      // Compute the active fragment when section is scrolled into view
+      this.intersectionObserver$ = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              this.activeRoute.set(entry.target.id);
+              console.log(this.activeRoute());
+            }
+          });
+        },
+        { threshold: 0.6 }
+      );
+
+      this.sections.forEach((section) => {
+        (this.intersectionObserver$ as IntersectionObserver).observe(
+          section.nativeElement
+        );
+      });
+    });
+  }
+
+  onMenuItemClick(route: string): void {
+    const section = this.sections.find((s) => s.nativeElement.id === route);
+    if (section) {
+      this.activeRoute.set(route);
+      section.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.intersectionObserver$) {
+      this.intersectionObserver$.disconnect();
+      this.intersectionObserver$ = undefined;
+    }
+  }
 }
+
+const NAV_ITEMS = [
+  {
+    label: 'Home',
+    route: 'home',
+  },
+  {
+    label: 'Details',
+    route: 'details',
+  },
+  {
+    label: 'RSVP',
+    route: 'rsvp',
+  },
+  {
+    label: 'FAQs',
+    route: 'faqs',
+  },
+  {
+    label: 'Registry',
+    route: 'registry',
+  },
+  {
+    label: 'Media',
+    route: 'media',
+  },
+];
