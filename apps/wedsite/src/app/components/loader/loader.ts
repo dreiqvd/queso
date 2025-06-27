248f5fc178fe7c049df4500e26c3bab62fabdd00
Drei Quevada <me@dreiq.dev>
2025-06-27 16:37:53 +0800

    wedsite: refactor loader component

--- apps/wedsite/src/app/components/loader/loader.ts
+++ apps/wedsite/src/app/components/loader/loader.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './loader.html',
 })
-export class Loader {}
+export class Loader {
+  readonly imageStyle = input<'transparent' | 'white'>('transparent');
+  readonly message = input<string>('');
+
+  protected readonly loaderImage = computed(() => {
+    return this.imageStyle() === 'transparent'
+      ? 'cat-sleep-1.svg'
+      : 'cat-sleep-2.svg';
+  });
+}
